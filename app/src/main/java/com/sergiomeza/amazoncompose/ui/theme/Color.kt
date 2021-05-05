d24c5937994d816e871f50133549ece02161c286
Sergio Meza <sergiomeza88@gmail.com>
2021-05-04 19:16:36 -0500

    Results

--- app/src/main/java/com/sergiomeza/amazoncompose/ui/theme/Color.kt
+++ app/src/main/java/com/sergiomeza/amazoncompose/ui/theme/Color.kt
@@ -2,11 +2,6 @@ package com.sergiomeza.amazoncompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val gradientHeader = listOf(Color(0xFF85D9E4), Color(0xFFDBF3FA))
 val gradientHeaderDark = listOf(Color(0xFF1D2F31), Color(0xFF272727))
 val locationHeader = Color(0xFFDBF3FA)
